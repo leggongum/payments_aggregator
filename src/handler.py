e338134f82b195073d26f0f6dd6395af20842a20
leggongum <arkad.tihonin@gmail.com>
2024-05-12 21:14:17 +0300

    bugfix

--- src/handler.py
+++ src/handler.py
@@ -20,11 +20,7 @@ async def get_start(message: Message):
 async def aggregate(message: Message):
     try:
         case = AggregationRequest(**json.loads(message.text))
-    except json.decoder.JSONDecodeError:
-        return await message.answer('Принимаются только сообщения вида: \n{\n"dt_from": isodate,\n"dt_upto": isodate,\n"group_type":"hour" or "day" or "week" or "month"\n}')
-    except pydantic_core._pydantic_core.ValidationError:
-        return await message.answer('Принимаются только сообщения вида: \n{\n"dt_from": isodate,\n"dt_upto": isodate,\n"group_type":"hour" or "day" or "week" or "month"\n}')
-    except TypeError:
+    except (json.decoder.JSONDecodeError, pydantic_core._pydantic_core.ValidationError,TypeError):
         return await message.answer('Принимаются только сообщения вида: \n{\n"dt_from": isodate,\n"dt_upto": isodate,\n"group_type":"hour" or "day" or "week" or "month"\n}')
 
     response = form_aggregation(case.model_dump())
