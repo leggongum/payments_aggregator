3f3929b2aa6fd19c8f6aed5b05a9e097d8fbac5d
leggongum <arkad.tihonin@gmail.com>
2024-05-12 19:36:42 +0300

    bugfix

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
+import pydantic_core
 
 import asyncio
 import json
@@ -20,6 +21,10 @@ async def aggregate(message: Message):
     try:
         case = AggregationRequest(**json.loads(message.text))
     except json.decoder.JSONDecodeError:
+        return await message.answer('Принимаются только сообщения вида: \n{\n"dt_from": isodate,\n"dt_upto": isodate,\n"group_type":"hour" or "day" or "week" or "month"\n}')
+    except pydantic_core._pydantic_core.ValidationError:
+        return await message.answer('Принимаются только сообщения вида: \n{\n"dt_from": isodate,\n"dt_upto": isodate,\n"group_type":"hour" or "day" or "week" or "month"\n}')
+    except TypeError:
         return await message.answer('Принимаются только сообщения вида: \n{\n"dt_from": isodate,\n"dt_upto": isodate,\n"group_type":"hour" or "day" or "week" or "month"\n}')
 
     response = form_aggregation(case.model_dump())
